dd1c94ce8f28158a6e3fae8a6a0275ce4433e5a1
monang241299 <monanglimbong42@gmail.com>
2022-12-23 19:57:26 +0700

    Commit Pertama

---
+++ app/src/main/java/com/example/tubes_alumgram/form_awal.kt
@@ -0,0 +1,11 @@
+package com.example.tubes_alumgram
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class form_awal : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_awal)
+    }
+}
---
+++ app/src/main/java/com/example/tubes_alumgram/home_page.kt
@@ -0,0 +1,11 @@
+package com.example.tubes_alumgram
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class home_page : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_page)
+    }
+}
